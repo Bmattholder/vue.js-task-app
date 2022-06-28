ae7f131ff43aa177aab928023eb2a58b775e14e9
Bmattholder <bmattholder@gmail.com>
2022-06-28 08:40:12 -0500

    adds button to task tracker and cleans init files

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h2>{{ title }}</h2>
+    <ButtonComponent text="Add Task" color="green"/>
+  </header>
+</template>
+
+<script>
+import ButtonComponent from './Button.vue'
+
+export default {
+  name: "HeaderComponent",
+  props: {
+    title: String,
+  },
+  components: {
+    ButtonComponent,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display-flex: space-between;
+  align-items: center;
+  margin-botton: 20px;
+}
+</style>
